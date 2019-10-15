5433e50f724d8d908eb81a33b0e18e94785e033c
Bethany Wride <bethanywride@Bethanys-MacBook-Air-2.local>
2019-10-15 13:57:12 -0600

    Finished model objects

--- Continuum/Continuum/Post.swift
+++ Continuum/Continuum/Post.swift
@@ -6,4 +6,38 @@
 //  Copyright © 2019 Bethany Bellio. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Post {
+    var photoData: Data?
+    let timestamp: Date
+    var caption: String
+    let comments: [Comment]
+    var photo: UIImage? {
+        get {
+            guard let photoData = photoData else { return nil }
+            return UIImage(data: photoData)
+        } set {
+            photoData = newValue?.jpegData(compressionQuality: 0.5)
+        }
+    }
+    
+    init(timestamp: Date = Date(), caption: String, comments: [Comment] = [], photo: UIImage?) {
+        self.timestamp = timestamp
+        self.caption = caption
+        self.comments = comments
+        self.photo = photo
+    }
+} // End of class
+
+class Comment {
+    var text: String
+    var timestamp: Date
+    weak var post: Post?
+    
+    init(text: String, timestamp: Date = Date(), post: Post?) {
+        self.text = text
+        self.timestamp = timestamp
+        self.post = post
+    }
+} // End of class
